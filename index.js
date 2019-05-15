64007a1bd76b2fb344f3ccc3ef721457a11f8ac1
Your Name <you@example.com>
2019-05-15 19:45:06 -0400

    AJAX + RESTful App

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ router.get('/hello', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/hello.html'));    
 });
 
+router.get('/quick', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/quick.html'));
+   });
+
+app.get('/api/text', (req, res) => {
+    if (req.query.text) {
+        res.status(200).send({
+            message: req.query.text.toUpperCase()
+        });
+    }
+    else
+    {
+        res.status(401).send();
+    }
+   
+});
+
+
 
 
 app.use('/', router);
